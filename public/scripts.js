531baf938973ae097a7df1394e40bc900084cae9
Yinuo Liu <77820812+YinuoLiu0708@users.noreply.github.com>
2023-05-05 23:33:37 -0400

    redo game.html

--- public/scripts.js
+++ public/scripts.js
@@ -2,21 +2,20 @@
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
 
-var shot;
-
 function play(){
     var rps = document.getElementById('rps');
     var rpsls = document.getElementById('rpsls');
     var opponent = document.getElementById('opponent');
+    
+    const base_url = window.location.href + 'app/';
+    const url;
 
     window.location.href = "game.html";
 
-    const base_url = window.location.href + 'app/';
-    const url = base_url + 'rps' + '/play' + '/' + shot;
-}
-
-function choose(choice){
-    shot = choice;
+    if (rps.checked){
+        $('.rpsls').hide();
+    }
+    
 }
 
 function viewRules(){
